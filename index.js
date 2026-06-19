4f17239592fb59db63a94dfe2179a9f0d1054b68
Marius <mariussangwa@gmail.com>
2026-06-19 19:27:00 +0200

    formatted the codes of the index file of the malog package

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // ANSI Color codes
-const {colors} = require("./colors")
+const { colors } = require("./colors")
 const malog = (req, res, next) => {
   const startTime = Date.now();
   const originalSend = res.send;
-  
-  res.send = function(data) {
+
+  res.send = function (data) {
     const responseTime = Date.now() - startTime;
     const status = res.statusCode;
     const method = req.method;
     const url = req.originalUrl;
-    
+
     let message = '';
     let statusColor;
-    
+
     if (status >= 200 && status < 300) {
       message = 'OK';
       statusColor = colors.green;
@@ -26,17 +26,17 @@ const malog = (req, res, next) => {
       message = 'SERVER_ERROR';
       statusColor = colors.red;
     }
-    
-    let timeColor = responseTime < 100 ? colors.green : 
-                    responseTime < 500 ? colors.yellow : colors.red;
-    
+
+    let timeColor = responseTime < 100 ? colors.green :
+      responseTime < 500 ? colors.yellow : colors.red;
+
     console.log(
       `${statusColor}[${status}]${colors.reset} ${colors.blue}${method}${colors.reset} ${colors.purple}${url}${colors.reset} ${colors.white}--${message}${colors.reset} ${timeColor}${responseTime}ms${colors.reset}`
     );
-    
+
     return originalSend.call(this, data);
   };
-  
+
   next();
 };
 
