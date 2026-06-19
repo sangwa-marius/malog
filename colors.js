417f1039c3bc0c448195ace40f505a73eb83aa09
Marius <mariussangwa@gmail.com>
2026-06-19 19:05:27 +0200

    added a color module

---
+++ colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    red:"\x1b[31m",
+    green:"\x1b[32m",
+    yellow:"\x1b[33m",
+    blue:"\x1b[34",
+    purple:"\x1b[35m",
+    lightBlue:"\x1b[36m",
+    white:"\x1b[37m",
+    set:"\x1b[0m"
+}
