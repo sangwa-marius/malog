3508b3be642250557cbc796d46107cebbb7bed26
Marius <mariussangwa@gmail.com>
2026-06-19 19:26:21 +0200

    formatted the codes of the colors module

--- colors.js
+++ colors.js
@@ -1,10 +1,10 @@
 export const colors = {
-    red:"\x1b[31m",
-    green:"\x1b[32m",
-    yellow:"\x1b[33m",
-    blue:"\x1b[34m",
-    purple:"\x1b[35m",
-    lightBlue:"\x1b[36m",
-    white:"\x1b[37m",
-    reset:"\x1b[0m"
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    blue: "\x1b[34m",
+    purple: "\x1b[35m",
+    lightBlue: "\x1b[36m",
+    white: "\x1b[37m",
+    reset: "\x1b[0m"
 }
